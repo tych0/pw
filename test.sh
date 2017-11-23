c01a70e9b02455dd0781d6bf9507ad211375153b
Tycho Andersen <tycho@tycho.ws>
2017-11-22 19:25:09 -0700

    add some tests
    Signed-off-by: Tycho Andersen <tycho@tycho.ws>

---
+++ test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+function pw() {
+    echo asdf | cargo run -- -q "$@"
+}
+
+set -x
+
+# test -d with a hand computed password
+[ "$(pw -r 180 zomg -d 2017-11-22)" = "LiV3cQBO0h" ]
+
+# test -r doesn't change too early
+for i in $(seq 1 10); do
+    [ "$(pw -r 10 zomg -d 2010-01-$i)" = "SQKt9Zgyyd" ]
+done
+[ "$(pw -r 10 zomg -d 2010-01-11)" = "CJ18ePduQ2" ]
+
+# test changing -r changes things
+[ "$(pw -r 10 zomg)" != "$(pw -r 20 zomg)" ]
+
+# test -o
+[ "$(pw zomg)" = "oXU35wO56X" ]
+[ "$(pw -o 0 zomg)" = "oXU35wO56X" ]
+[ "$(pw -o 1 zomg)" = "Kr54O/5mh7" ]
+[ "$(pw -o 2 zomg)" = "i24SkBFkm4" ]
+
+# test -s
+[ "$(pw zomg -s)" = "XU35wO56X^" ]
+
+# test -l
+[ "$(pw -l 20 zomg)" = "oXU35wO56XLbISexn4pT" ]
+
+# test -l -s
+[ "$(pw zomg -l 20 -s)" = "XU35wO56XLbISexn4pT{" ]
