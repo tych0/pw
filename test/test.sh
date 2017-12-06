ac697f8a3ccafcef5cf3b7ab7dfa6976d22980c6
Tycho Andersen <tycho@tycho.ws>
2017-12-05 20:59:48 -0700

    update tests for recent algorithm changes
    recent commits updated the algorithm and some default settings. This
    updates the tests accordingly.
    
    Signed-off-by: Tycho Andersen <tycho@tycho.ws>

--- test/test.sh
+++ test/test.sh
@@ -7,52 +7,54 @@ function pw() {
 }
 
 function cleanup() {
+    set +x
     if [ -n "$TRAVIS" ]; then
         return;
     fi
-    cargo run -- -u pw-test-user --delete-keyring-password
-    cargo run -- -u pw-test-user --delete-keyring-config
+    cargo run -- -u pw-test-user --delete-keyring-password >& /dev/null
+    cargo run -- -u pw-test-user --delete-keyring-config >& /dev/null
+    rm ./from_keyring.toml >& /dev/null || true
 }
 trap cleanup EXIT HUP INT TERM
 
 set -x
 
 # test -d with a hand computed password
-[ "$(pw -r 180 zomg -d 2017-11-22)" = "LiV3cQBO0h" ]
+[ "$(pw -r 180 zomg -d 2017-11-22)" = "56FAdSQuVAJEKbZmjJ3E" ]
 
 # test -r doesn't change too early
 for i in $(seq 1 10); do
-    [ "$(pw -r 10 zomg -d 2010-01-$i)" = "SQKt9Zgyyd" ]
+    [ "$(pw -r 10 zomg -d 2010-01-$i)" = "2kKQp5YZhBNqNB6Vfvfv" ]
 done
-[ "$(pw -r 10 zomg -d 2010-01-11)" = "CJ18ePduQ2" ]
+[ "$(pw -r 10 zomg -d 2010-01-11)" = "33zajgdUXdYdxM1Vf8Uw" ]
 
 # test changing -r changes things
 [ "$(pw -r 10 zomg)" != "$(pw -r 20 zomg)" ]
 
 # test -o
-[ "$(pw zomg)" = "oXU35wO56X" ]
-[ "$(pw -o 0 zomg)" = "oXU35wO56X" ]
-[ "$(pw -o 1 zomg)" = "Kr54O/5mh7" ]
-[ "$(pw -o 2 zomg)" = "i24SkBFkm4" ]
+[ "$(pw zomg)" = "35iLqmvYkvn1XiHx79p8" ]
+[ "$(pw -o 0 zomg)" = "35iLqmvYkvn1XiHx79p8" ]
+[ "$(pw -o 1 zomg)" = "u8iiCCb3jxUuneoUZz74" ]
+[ "$(pw -o 2 zomg)" = "5tkUZg7jmr6WpmG73oa5" ]
 
 # test -s
-[ "$(pw zomg -s)" = "XU35wO56X^" ]
+[ "$(pw zomg -s)" = "3[iLqmvYkvn1XiHx79p8" ]
 
 # test -l
-[ "$(pw -l 20 zomg)" = "oXU35wO56XLbISexn4pT" ]
+[ "$(pw -l 30 zomg)" = "35iLqmvYkvn1XiHx79p8XohdTEvDTQ" ]
 
 # test -l -s
-[ "$(pw zomg -l 20 -s)" = "XU35wO56XLbISexn4pT{" ]
+[ "$(pw zomg -l 30 -s)" = "35iLq]vYkvn1XiHx79p8XohdTEvDTQ" ]
 
 # test otp config fallback
-[ "$(pw -f ./test.toml zomg)" = "Kr54O/5mh7" ]
-[ "$(pw -f ./test.toml zomg -o 2)" = "i24SkBFkm4" ]
+[ "$(pw -f ./test.toml zomg)" = "u8iiCCb3jxUuneoUZz74" ]
+[ "$(pw -f ./test.toml zomg -o 2)" = "5tkUZg7jmr6WpmG73oa5" ]
 
 # test --question
-[ "$(pw zomg --question "Do you like trees?")" = "H/zJqV24Gb" ]
+[ "$(pw zomg --question "Do you like trees?")" = "4ikXexSJLsWYd3Wu1DYy" ]
 
 # test preshared config option
-[ "$(pw zomg -f ./preshared.toml)" = "o+uxxezJBT" ]
+[ "$(pw zomg -f ./preshared.toml)" = "4TGjqSEHv1ve9KXJunnb" ]
 
 
 # Below here we can't test on travis, because it doesn't have secretservice.
@@ -60,16 +62,17 @@ if [ -n "$TRAVIS" ]; then
     exit 0
 fi
 
-pw --set-keyring-password
+echo -e "asdf\nasdf" | cargo run -- -q -u pw-test-user --set-keyring-password
 [ "$(pw --get-keyring-password)" == "asdf" ]
 
 # explicitly use cargo run, so we can be sure to bypass entering the password
-[ "$(cargo run -- zomg)" = "oXU35wO56X" ]
+[ "$(cargo run -- -u pw-test-user zomg)" = "35iLqmvYkvn1XiHx79p8" ]
 
 # test that we don't mutliate the config
 pw --set-keyring-config ./keyring.toml
 pw --get-keyring-config ./from_keyring.toml
 [ "$(sha256sum ./keyring.toml | cut -f1 -d" ")" == "$(sha256sum ./from_keyring.toml | cut -f1 -d" ")" ]
+rm ./from_keyring.toml
 
 # now test that we got the right otp=1 password from the keyring config
-[ "$(pw zomg)" = "Kr54O/5mh7" ]
+[ "$(pw zomg)" = "u8iiCCb3jxUuneoUZz74" ]
